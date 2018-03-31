53121a754934c7a87f5160c7f47bc0a523a2f890
RyKishita <rykishita@gmail.com>
2018-03-31 21:18:59 +0900

    ・名前付きパイプはIN・OUT両方指定ができるが、Unity側で読み書きが競合してしまうので分ける。 ・タイマーでピア情報取得 後Unity側が出来れば完成するものと思われる。

--- RelationUnity/Program.cs
+++ RelationUnity/Program.cs
@@ -10,32 +10,32 @@ namespace RelationUnity
 {
     class Program
     {
+        const int buffersize = 4096;
+
         static async Task Main(string[] args)
         {
-            if (args.Length != 4)
-            {
-#if DEBUG
-                Console.WriteLine("invalid arguments");
-                Console.ReadLine();
-#endif
-                return;
-            }
-
-            string pipeName = args[0];
-            string connectionTarget = args[1];
-            string portNoStr = args[2];
-            string param3 = args[3];
-
-#if DEBUG
-            Console.WriteLine($"{nameof(pipeName)}={pipeName},{nameof(connectionTarget)}={connectionTarget},{nameof(portNoStr)}={portNoStr},{nameof(param3)}={param3}");
-#endif
-
-            int portNo;
-            PeerName peerName;
-            Cloud cloud;
             try
             {
-                portNo = int.Parse(portNoStr);
+                if (args.Length != 4)
+                {
+#if DEBUG
+                    Console.WriteLine("invalid arguments");
+                    Console.ReadLine();
+#endif
+                    return;
+                }
+
+                string pipeName = args[0];
+                string connectionTarget = args[1];
+                string portNoStr = args[2];
+                string param3 = args[3];
+#if DEBUG
+                Console.WriteLine($"{nameof(pipeName)}={pipeName},{nameof(connectionTarget)}={connectionTarget},{nameof(portNoStr)}={portNoStr},{nameof(param3)}={param3}");
+#endif
+
+                int portNo = int.Parse(portNoStr);
+                PeerName peerName = null;
+                Cloud cloud = null;
 
                 switch (connectionTarget.ToLower())
                 {
@@ -50,89 +50,115 @@ namespace RelationUnity
                     default:
                         throw new ArgumentException("invalid argument", "param 1");
                 }
+
+                using (var sendNamedPipe = new NamedPipeClientStream(".", pipeName + "i"))//, PipeDirection.InOut, PipeOptions.Asynchronous))
+                {
+                    await sendNamedPipe.ConnectAsync();
+
+                    using (var receiveNamedPipe = new NamedPipeServerStream(pipeName + "o"))//, PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous, buffersize, buffersize))
+                    {
+                        await receiveNamedPipe.WaitForConnectionAsync();
+
+                        var peerNameResolver = new PeerNameResolver();
+
+                        using (var timer = new System.Timers.Timer(1000))
+                        {
+                            timer.Elapsed += (sender, e) =>
+                            {
+                                timer.Stop();
+                                peerNameResolver.ResolveAsync(peerName, cloud, Guid.NewGuid());
+#if DEBUG
+                                Console.WriteLine("ResolveAsync");
+#endif
+                            };
+                            void resolveProgressChanged(object sender, ResolveProgressChangedEventArgs e)
+                            {
+                                sendNamedPipe.Write(e.PeerNameRecord.Data, 0, e.PeerNameRecord.Data.Length);
+                                sendNamedPipe.Flush();
+#if DEBUG
+                                //var buffer = e.PeerNameRecord.Data;
+                                //var length = e.PeerNameRecord.Data.Length;
+                                //Console.WriteLine($"{nameof(resolveProgressChanged)} {nameof(buffer)}[{length}]={BitConverter.ToString(buffer, 0, length).Replace("-", string.Empty)}");
+                                Console.WriteLine(nameof(resolveProgressChanged));
+#endif
+                            }
+
+                            void resolveCompleted(object sender, ResolveCompletedEventArgs e)
+                            {
+                                foreach (var record in e.PeerNameRecordCollection)
+                                {
+                                    sendNamedPipe.Write(record.Data, 0, record.Data.Length);
+                                    sendNamedPipe.Flush();
+                                }
+#if DEBUG
+                                Console.WriteLine($"{nameof(resolveCompleted)} count={e.PeerNameRecordCollection.Count}");
+#endif
+                                timer.Start();
+                            }
+
+                            peerNameResolver.ResolveProgressChanged += resolveProgressChanged;
+                            peerNameResolver.ResolveCompleted += resolveCompleted;
+
+                            timer.Start();
+
+                            try
+                            {
+                                var peerNameRegistration = new PeerNameRegistration(peerName, portNo, cloud);
+
+                                while (true)
+                                {
+                                    var buffer = new byte[buffersize];
+                                    int length = await receiveNamedPipe.ReadAsync(buffer, 0, buffer.Length);
+
+                                    if (0 == length)
+                                    {
+                                        ;
+                                    }
+                                    else if (1 == length)
+                                    {
+                                        // 終了コード
+                                        if (buffer[0] == 0xFF) break;
+                                    }
+                                    else
+                                    {
+#if DEBUG
+                                        //Console.WriteLine($"Read {nameof(buffer)}[{length}]={BitConverter.ToString(buffer, 0, length).Replace("-", string.Empty)}");
+                                        Console.WriteLine("Read");
+#endif
+                                        var bufferSend = new byte[length];
+                                        Buffer.BlockCopy(buffer, 0, bufferSend, 0, length);
+                                        peerNameRegistration.Data = bufferSend;
+                                        if (peerNameRegistration.IsRegistered())
+                                        {
+                                            peerNameRegistration.Update();
+                                        }
+                                        else
+                                        {
+                                            peerNameRegistration.Start();
+                                        }
+                                    }
+                                }
+                            }
+                            finally
+                            {
+                                timer.Stop();
+                                peerNameResolver.ResolveProgressChanged -= resolveProgressChanged;
+                                peerNameResolver.ResolveCompleted -= resolveCompleted;
+                            }
+                        }
+                    }
+                }
             }
             catch (Exception ex)
             {
 #if DEBUG
                 Console.WriteLine(ex);
-                Console.ReadLine();
-#endif
-                return;
-            }
-
-            server = new NamedPipeServerStream(pipeName);
-            try
-            {
-                await server.WaitForConnectionAsync();
-
-                var peerNameResolver = new PeerNameResolver();
-                peerNameResolver.ResolveProgressChanged += PeerNameResolver_ResolveProgressChanged;
-                peerNameResolver.ResolveCompleted += PeerNameResolver_ResolveCompleted;
-
-                peerNameResolver.ResolveAsync(peerName, cloud);
-
-                var peerNameRegistration = new PeerNameRegistration(peerName, portNo, cloud);
-
-                while (true)
-                {
-                    var buffer = new byte[4096];
-                    int length = await server.ReadAsync(buffer, 0, 4096);
-
-                    if (0 == length) continue;
-#if DEBUG
-                    Console.WriteLine($"{nameof(buffer)}[{length}]={BitConverter.ToString(buffer, 0, length).Replace("-", string.Empty)}");
-#endif
-                    // 終了コード
-                    if (1 == length && buffer[0] == 0xFF) break;
-
-                    peerNameRegistration.Data = buffer;
-                    if (peerNameRegistration.IsRegistered())
-                    {
-                        peerNameRegistration.Update();
-                    }
-                    else
-                    {
-                        peerNameRegistration.Start();
-                    }
-                }
-
-                peerNameResolver.ResolveProgressChanged -= PeerNameResolver_ResolveProgressChanged;
-                peerNameResolver.ResolveCompleted -= PeerNameResolver_ResolveCompleted;
-            }
-            catch(Exception ex)
-            {
-#if DEBUG
-                Console.WriteLine(ex);
 #endif
             }
-            finally
-            {
 #if DEBUG
-                Console.WriteLine($"server.Close();");
-#endif
-                server.Close();
-            }
-
-#if DEBUG
+            Console.WriteLine($"end");
             Console.ReadLine();
 #endif
-        }
-
-        static NamedPipeServerStream server = null;
-
-        private static void PeerNameResolver_ResolveProgressChanged(object sender, ResolveProgressChangedEventArgs e)
-        {
-            server.Write(e.PeerNameRecord.Data, 0, e.PeerNameRecord.Data.Length);
-            server.Flush();
-        }
-
-        private static void PeerNameResolver_ResolveCompleted(object sender, ResolveCompletedEventArgs e)
-        {
-            foreach(var record in e.PeerNameRecordCollection)
-            {
-                server.Write(record.Data, 0, record.Data.Length);
-                server.Flush();
-            }
         }
     }
 }
