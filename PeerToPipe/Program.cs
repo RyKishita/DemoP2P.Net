a2fea48fa14e0798417ae80a9c27db55900f7ca3
RyKishita <rykishita@gmail.com>
2018-04-07 12:29:44 +0900

    送信・受信を分割。また、作った理由はUnityで使う為ではあるが、MonoでP2Pが使えないのが原因である為名前にUnityを含めないようにした。

---
+++ PeerToPipe/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Pipes;
+using System.Net.PeerToPeer;
+
+namespace PeerToPipe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 4)
+                {
+#if DEBUG
+                    Console.WriteLine("invalid arguments");
+                    Console.ReadLine();
+#endif
+                    return;
+                }
+
+                string pipeName = args[0];
+                string connectionTarget = args[1];
+                string portNoStr = args[2];
+                string param3 = args[3];
+#if DEBUG
+                Console.WriteLine($"{nameof(pipeName)}={pipeName},{nameof(connectionTarget)}={connectionTarget},{nameof(portNoStr)}={portNoStr},{nameof(param3)}={param3}");
+#endif
+
+                int portNo = int.Parse(portNoStr);
+                PeerName peerName = null;
+                Cloud cloud = null;
+
+                switch (connectionTarget.ToLower())
+                {
+                    case "global":
+                        cloud = Cloud.Global;
+                        peerName = PeerName.CreateFromPeerHostName(param3);
+                        break;
+                    case "local":
+                        cloud = Cloud.AllLinkLocal;
+                        peerName = new PeerName(param3, PeerNameType.Secured);
+                        break;
+                    default:
+                        throw new ArgumentException("invalid argument", "param 1");
+                }
+
+                using (var namedPipePipeToPeer = new NamedPipeClientStream(".", pipeName + "PipeToPeer"))
+                {
+                    namedPipePipeToPeer.Connect();
+#if DEBUG
+                    Console.WriteLine("connect named pipe 'PipeToPeer'");
+#endif
+                    using (var namedPipePeerToPipe = new NamedPipeClientStream(".", pipeName + "PeerToPipe"))
+                    {
+                        namedPipePeerToPipe.Connect();
+#if DEBUG
+                        Console.WriteLine("connect named pipe 'PeerToPipe'");
+#endif
+                        var peerNameResolver = new PeerNameResolver();
+
+                        while (namedPipePipeToPeer.IsConnected && namedPipePeerToPipe.IsConnected)
+                        {
+                            int result = namedPipePipeToPeer.ReadByte();
+                            if (result < 0) throw new Exception("end of stream");
+#if DEBUG
+                            Console.WriteLine("read");
+#endif
+                            // 読み込み実行
+                            if (result == 0xFF)
+                            {
+#if DEBUG
+                                Console.WriteLine("resolve start");
+#endif
+                                foreach (var record in peerNameResolver.Resolve(peerName, cloud))
+                                {
+                                    namedPipePeerToPipe.Write(record.Data, 0, record.Data.Length);
+                                    namedPipePeerToPipe.Flush();
+                                    namedPipePeerToPipe.WaitForPipeDrain();
+#if DEBUG
+                                    var buffer = record.Data;
+                                    var length = record.Data.Length;
+                                    Console.WriteLine($"{nameof(buffer)}[{length}]={BitConverter.ToString(buffer, 0, length).Replace("-", string.Empty)}");
+#endif
+                                }
+
+                                namedPipePeerToPipe.WriteByte(0xFF);//終端
+                                namedPipePeerToPipe.Flush();
+                                namedPipePeerToPipe.WaitForPipeDrain();
+
+#if DEBUG
+                                Console.WriteLine("resolve end");
+#endif
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                Console.WriteLine(ex);
+                Console.ReadLine();
+#endif
+            }
+#if DEBUG
+            Console.WriteLine("end");
+#endif
+        }
+    }
+}
---
+++ PipeToPeer/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Pipes;
+using System.Net.PeerToPeer;
+
+namespace PipeToPeer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 4)
+                {
+#if DEBUG
+                    Console.WriteLine("invalid arguments");
+                    Console.ReadLine();
+#endif
+                    return;
+                }
+
+                string pipeName = args[0];
+                string connectionTarget = args[1];
+                string portNoStr = args[2];
+                string param3 = args[3];
+#if DEBUG
+                Console.WriteLine($"{nameof(pipeName)}={pipeName},{nameof(connectionTarget)}={connectionTarget},{nameof(portNoStr)}={portNoStr},{nameof(param3)}={param3}");
+#endif
+
+                int portNo = int.Parse(portNoStr);
+                PeerName peerName = null;
+                Cloud cloud = null;
+
+                switch (connectionTarget.ToLower())
+                {
+                    case "global":
+                        cloud = Cloud.Global;
+                        peerName = PeerName.CreateFromPeerHostName(param3);
+                        break;
+                    case "local":
+                        cloud = Cloud.AllLinkLocal;
+                        peerName = new PeerName(param3, PeerNameType.Secured);
+                        break;
+                    default:
+                        throw new ArgumentException("invalid argument", "param 1");
+                }
+
+                using (var namedPipeRead = new NamedPipeClientStream(".", pipeName, PipeDirection.In, PipeOptions.Asynchronous))
+                {
+                    namedPipeRead.Connect();
+#if DEBUG
+                    Console.WriteLine("connect named pipe");
+#endif
+                    var peerNameRegistration = new PeerNameRegistration(peerName, portNo, cloud);
+                    try
+                    {
+                        while (namedPipeRead.IsConnected)
+                        {
+                            var readBuffer = new byte[4096];
+                            int length = namedPipeRead.Read(readBuffer, 0, readBuffer.Length);
+#if DEBUG
+                            //Console.WriteLine($"read {nameof(buffer)}[{length}]={BitConverter.ToString(buffer, 0, length).Replace("-", string.Empty)}");
+                            Console.WriteLine("read");
+#endif
+                            if (0 == length)
+                            {
+                                ;
+                            }
+                            else
+                            {
+#if DEBUG
+                                Console.WriteLine("regist start");
+#endif
+                                var registBuffer = new byte[length];
+                                Buffer.BlockCopy(readBuffer, 0, registBuffer, 0, length);
+                                peerNameRegistration.Data = registBuffer;
+                                if (peerNameRegistration.IsRegistered())
+                                {
+                                    peerNameRegistration.Update();
+                                }
+                                else
+                                {
+                                    peerNameRegistration.Start();
+                                }
+#if DEBUG
+                                Console.WriteLine("regist end");
+#endif
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        peerNameRegistration.Stop();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                Console.WriteLine(ex);
+                Console.ReadLine();
+#endif
+            }
+#if DEBUG
+            Console.WriteLine("end");
+#endif
+        }
+    }
+}
